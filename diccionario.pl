c9ffceefe52a5938100e16aeb5f5e6287e219b75
Javier Junquera Sánchez <javier.junquera.sanchez@gmail.com>
2016-04-18 23:45:47 +0200

    Funcionan todas las frases en castellano. Hay que comprobar errores (y chapucillas...).

--- diccionario.pl
+++ diccionario.pl
@@ -10,6 +10,8 @@ determinante(det(det_8), f, pl) --> [unas].
 determinante(det(det_9), m, pl) --> [ellos].
 determinante(det(det_10), m, sn) --> [tu].
 determinante(det(det_10), f, sn) --> [tu].
+determinante(det(det_11), m, sn) --> [mi].
+determinante(det(det_11), f, sn) --> [mi].
 
 nombre(n(n_1), m, sn) --> [hombre].
 nombre(n(n_2), f, sn) --> [mujer].
@@ -22,14 +24,29 @@ nombre(n(n_8), m, sn) --> [raton].
 nombre(n(n_9), m, sn) --> [alumno].
 nombre(n(n_10), m, pl) --> [ratones].
 nombre(n(n_11), f, sn) --> [universidad].
+nombre(n(n_12), m, sn) --> [perro].
+nombre(n(n_13), m, sn) --> [jardin].
+nombre(n(n_14), m, sn) --> [vecino].
+nombre(n(n_15), m, sn) --> [canario].
+nombre(n(n_16), m, sn) --> [periodico].
+nombre(n(n_17), m, sn) --> [cafe].
+nombre(n(n_18), m, sn) --> [oscar, wilde].
+nombre(n(n_19), m, sn) --> [el, fantasma, de, canterville].
 
 verbo(v(v_1), sn) --> [ama].
 verbo(v(v_2), sn) --> [come].
 verbo(v(v_2), sn) --> [comes].
 verbo(v(v_3), sn) --> [estudia].
 verbo(v(v_4), sn) --> [era].
-verbo(v(v_5), sn) --> [cazó].
+verbo(v(v_5), sn) --> [cazo].
 verbo(v(v_6), sn) --> [es].
+verbo(v(v_7), pl) --> [comen].
+verbo(v(v_8), sn) --> [persiguio].
+verbo(v(v_8), pl) --> [vimos].
+verbo(v(v_9), sn) --> [canta].
+verbo(v(v_10), sn) --> [toma].
+verbo(v(v_11), sn) --> [lee].
+verbo(v(v_12), sn) --> [escribio].
 
 adjetivo(a(a_1), f, sn) --> [roja].
 adjetivo(a(a_2), m, sn) --> [negro].
@@ -38,6 +55,10 @@ adjetivo(a(a_3), f, sn) --> [gris].
 adjetivo(a(a_4), m, sn) --> [pequeño].
 adjetivo(a(a_5), m, sn) --> [grande].
 adjetivo(a(a_5), f, sn) --> [grande].
+adjetivo(a(a_6), m, sn) --> [bien].
+adjetivo(a(a_7), m, sn) --> [amarillo].
+adjetivo(a(a_8), m, sn) --> [delgado].
+adjetivo(a(a_6), f, sn) --> [alta].
 
 preposicion(p(p_1)) --> [a].
 preposicion(p(p_2)) --> [ante].
@@ -62,3 +83,8 @@ preposicion(p(p_20)) --> [mediante].
 
 
 pronombre(pn(pn_1)) --> [que].
+
+adverbio(adv(adv_1)) --> [ayer].
+adverbio(adv(adv_2)) --> [muy].
+
+conjuncion(cnj(cnj_1)) --> [y].
--- gramatica.pl
+++ gramatica.pl
@@ -1,17 +1,38 @@
 % Reglas gramaticales
 oracion(o(GN, GV)) --> g_nominal(GN, _, NUM), g_verbal(GV, NUM).
+oracion(o(GV)) --> g_verbal(GV, _).
 
 g_nominal(gn(N), G, NUM) --> nombre(N, G, NUM).
+g_nominal(gn(N), G, NUM) --> determinante(N, G, NUM).
 g_nominal(gn(D, N), G, NUM) --> determinante(D, G, NUM), nombre(N, G, NUM).
 g_nominal(gn(N, A), G, NUM) --> nombre(N, G, NUM), adjetivo(A, G, NUM).
 g_nominal(gn(D, N, A), G, NUM) --> determinante(D, G, NUM), nombre(N, G, NUM), adjetivo(A, G, NUM).
 g_nominal(gn(D, GP), G, NUM) --> determinante(D, G, NUM), g_proposicional(GP).
 g_nominal(gn(N, GP), G, NUM) --> nombre(N, G, NUM), g_proposicional(GP).
+g_nominal(gn(D, N, A, GP), G, NUM) --> determinante(D, G, NUM), nombre(N, G, NUM), adjetivo(A, G, NUM), g_preposicional(GP).
+g_nominal(gn(D, N, GP), G, NUM) --> determinante(D, G, NUM), nombre(N, G, NUM), g_preposicional(GP).
 g_nominal(gn(D, N, GP), G, NUM) --> determinante(D, G, NUM), nombre(N, G, NUM), g_proposicional(GP).
+
+% g_proposicional(gp(PN)).
 g_proposicional(gp(PN, GV)) --> pronombre(PN), g_verbal(GV, _).
+
 g_preposicional(gp(P, GN)) --> preposicion(P), g_nominal(GN, _, _).
+g_preposicional(gp(P, GV)) --> preposicion(P), g_verbal(GV, _).
+g_preposicional(gp(ADV, GN)) --> adverbio(ADV), g_nominal(GN, _, _).
+g_preposicional(gp(ADV, GV)) --> adverbio(ADV), g_verbal(GV, _).
+
+g_adverbial(ga(ADV)) --> adverbio(ADV).
+g_adverbial(ga(ADV, A)) --> adverbio(ADV), adjetivo(A, _, _).
+g_adverbial(ga(ADV, A, GN)) --> adverbio(ADV), adjetivo(A, G, NUM), g_nominal(GN, G, NUM).
+
+compuesta(cnj(CNJ, O)) --> conjuncion(CNJ), oracion(O).
 
 g_verbal(gv(V), NUM) --> verbo(V,NUM).
+g_verbal(gv(V, GN, CMP), NUM) --> verbo(V,NUM), g_nominal(GN, _, _), compuesta(CMP).
 g_verbal(gv(V, GN), NUM) --> verbo(V,NUM), g_nominal(GN, _, NUM).
 g_verbal(gv(V, A), NUM) --> verbo(V,NUM), adjetivo(A, _, NUM).
+g_verbal(gv(V, A, GA), NUM) --> verbo(V,NUM), adjetivo(A, _, NUM), g_adverbial(GA).
 g_verbal(gv(V, GP), NUM) --> verbo(V,NUM), g_preposicional(GP).
+g_verbal(gv(V, GA), NUM) --> verbo(V,NUM), g_adverbial(GA).
+g_verbal(gv(V, A, CMP), NUM) --> verbo(V,NUM), adjetivo(A, _, NUM), compuesta(CMP).
+g_verbal(gv(V, CMP), NUM) --> verbo(V,NUM), compuesta(CMP).
